75e3f4bbb0a7758aeb03964c9b6addfc35261682
Jasonlee6789 <jeffersonlee@163.com>
2020-11-08 11:46:56 +0800

    开始造时间侣行的功能，比较惊奇于王者荣耀在录像功能。

--- src/index.js
+++ src/index.js
@@ -94,6 +94,17 @@ function Square(props) {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
