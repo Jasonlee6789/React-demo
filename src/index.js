c371623986de1871b8f4be6ae277e38dcbb6e0bb
Jasonlee6789 <jeffersonlee@163.com>
2020-11-08 10:28:24 +0800

    I modified the Square to be a function component.

--- src/index.js
+++ src/index.js
@@ -49,25 +49,33 @@ class Board extends React.Component {
   }
 }
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { value: null };
-  // }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          // this.setState({ value: "X" });
-          this.props.onClick();
-        }}
-      >
-        {/* {this.state.value} */}
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = { value: null };
+//   // }
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => {
+//           // this.setState({ value: "X" });
+//           this.props.onClick();
+//         }}
+//       >
+//         {/* {this.state.value} */}
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Game extends React.Component {
