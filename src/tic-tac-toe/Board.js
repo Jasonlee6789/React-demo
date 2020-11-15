839cb62b7b68c2ff22662ceaea5f39dfc1d4db95
Jasonlee6789 <jeffersonlee@163.com>
2020-11-15 11:59:21 +0800

    我在Board.js里面的row里面删除 columns={props.game.size}，并且写了导航Menu模块.

--- src/tic-tac-toe/Board.js
+++ src/tic-tac-toe/Board.js
@@ -5,26 +5,26 @@ import _ from "lodash";
 
 function Board(props) {
   const rows = _.times(props.game.size, (row) => (
-    <Grid.Row columns={props.game.size}>{cols(row)}</Grid.Row>
+    <Grid.Row>{cols(row)}</Grid.Row>
   ));
-}
 
-function cols(row) {
-  const items = [];
+  function cols(row) {
+    const items = [];
 
-  for (let col = 0; col < props.game.size; col++) {
-    let id = row * props.game.size + col;
-    items.push(
-      <Grid.Column width={1}>
-        <Square
-          value={props.game.board[id]}
-          onClick={() => props.onClick(id)}
-        />
-      </Grid.Column>
-    );
+    for (let col = 0; col < props.game.size; col++) {
+      let id = row * props.game.size + col;
+      items.push(
+        <Grid.Column width={1}>
+          <Square
+            value={props.game.board[id]}
+            onClick={() => props.onClick(id)}
+          />
+        </Grid.Column>
+      );
+    }
+
+    return items;
   }
-
-  return items;
 }
 
 export default Board;
