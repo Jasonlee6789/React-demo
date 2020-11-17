38a7ff69dab02b9349fefb27346d18e4fa61fe56
Jasonlee6789 <jeffersonlee@163.com>
2020-11-17 17:23:32 +0800

    我更新了Board.js一些细节。

--- src/tic-tac-toe/Board.js
+++ src/tic-tac-toe/Board.js
@@ -7,20 +7,18 @@ function Board(props) {
   // const row = (id / game.size) >> 0;
   // const col = id % game.size;
 
-  const rows = _.times(props.game.size, (row) => (
-    <Grid.Row>{cols(row)}</Grid.Row>
-  ));
+  const rows = _.times(props.size, (row) => <Grid.Row>{cols(row)}</Grid.Row>);
 
   function cols(row) {
     const items = [];
 
-    for (let col = 0; col < props.game.size; col++) {
-      let id = row * props.game.size + col;
+    for (let col = 0; col < props.size; col++) {
+      let id = row * props.size + col;
 
       items.push(
         <Grid.Column width={1}>
           <Square
-            value={props.game.board[id]}
+            value={props.board && props.board[id]}
             onClick={() => props.onClick(id)}
           />
         </Grid.Column>
@@ -29,6 +27,11 @@ function Board(props) {
 
     return items;
   }
+  return (
+    <Container style={{ margin: 20 }}>
+      <Grid centered>{rows}</Grid>
+    </Container>
+  );
 }
 
 export default Board;
